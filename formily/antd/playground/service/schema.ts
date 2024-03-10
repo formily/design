1a49f2a10e1dc4a48db5c0a571a43868ada90c65
dev-zuo <i@zuoguoqing.com>
2024-03-10 23:49:06 +0800

    feat: add bid save 基础逻辑

--- formily/antd/playground/service/schema.ts
+++ formily/antd/playground/service/schema.ts
@@ -6,6 +6,16 @@ import {
 import { message } from 'antd'
 
 export const saveSchema = (designer: Engine) => {
+  let queryParams = Object.fromEntries(new URLSearchParams(location.search))
+  let { bid } = queryParams
+  if (!bid) {
+    message.warning('url 异常，缺少 id 参数')
+    return
+  }
+  let formilySchema = JSON.stringify(
+    transformToSchema(designer.getCurrentTree())
+  )
+  console.log('formily-schema', formilySchema)
   localStorage.setItem(
     'formily-schema',
     JSON.stringify(transformToSchema(designer.getCurrentTree()))
